743e3084cb0b14c5938be4a32a2fe3262851ef6e
Anson Thomas <thomasanson53@gmail.com>
2026-06-14 22:22:02 +0530

    new script added to extract lobster charges to xyz file

---
+++ lobster_charge_extract/conv-lob-charges.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+##Script to quickly convert LOBSTER-computed charges to an extxyz structure with charges read from CHARGE.lobster file
+##Usage: python conv-lob-charges.py
+import sys,os
+import numpy as np
+from ase.io import read, write
+from pymatgen.io.lobster.outputs import Charge
+
+# ASE reads QE input files directly
+atoms = read('POSCAR.lobster.vasp')
+
+# read ACF.dat — skip 2 header lines, drop last 4 summary lines
+charge = Charge(filename='CHARGE.lobster')
+mulliken=charge.mulliken
+loewdin=charge.loewdin
+
+atoms.set_initial_charges(mulliken)
+filename='mulliken'+'.xyz'
+print(filename)
+atoms.write(filename)
+print(f"Written: {filename}.xyz")
+
+atoms.set_initial_charges(loewdin)
+filename='loewdin'+'.xyz'
+print(filename)
+atoms.write(filename)
+print(f"Written: {filename}.xyz")
